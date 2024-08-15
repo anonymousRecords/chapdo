488d842fd917f6415843888281d5d386678e3b09
anonymousrecords <a.Records2022@gmail.com>
2024-08-16 00:43:27 +0900

    chore: 임시 사진 추가

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,9 @@
+import Image from "next/image";
 
 export default function Home() {
   return (
     <main>
-
+      <Image src="/main.webp" alt="main" width={2000} height={200} />
     </main>
   );
 }
