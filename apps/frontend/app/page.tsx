b8954e5ebb3dc13227ea8dcbcd4fd55e657d98ab
anonymousrecords <a.Records2022@gmail.com>
2024-11-20 15:55:06 +0900

    feat: Implement Carousel

--- apps/frontend/app/page.tsx
+++ apps/frontend/app/page.tsx
@@ -1,19 +1,20 @@
-const snsList = [
+import Carousel from '@/components/ui/carousel/carousel';
+
+const SNSLIST = [
   { id: 1, name: 'Github', url: 'https://www.facebook.com/' },
   { id: 2, name: 'Twitter', url: 'https://www.twitter.com/' },
   { id: 3, name: 'LinkedIn', url: 'https://www.facebook.com/' },
   { id: 4, name: 'Instagram', url: 'https://www.facebook.com/' },
-];
-
-const TITLE = 'CHAPDO';
+] as const;
 
 export default function Home() {
   return (
     <main className="h-full bg-fuchsia-200">
       <section className="flex flex-col justify-center items-center">
-        <h1>{TITLE}</h1>
+        <Carousel />
+        <h1>안녕하세요. 찹도입니다.</h1>
         <nav>
-          {snsList.map((sns) => (
+          {SNSLIST.map((sns) => (
             <li key={sns.id}>
               <a
                 key={sns.id}
