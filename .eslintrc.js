6c5d37c037d4a17b2ad6df410b3793d3fcaffa1c
anonymousrecords <a.Records2022@gmail.com>
2024-11-22 01:16:54 +0900

    chore: approve any type

--- .eslintrc.js
+++ .eslintrc.js
@@ -26,6 +26,7 @@ module.exports = {
     'react/react-in-jsx-scope': 'off',
     'react/prop-types': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
   },
   settings: {
     react: {
