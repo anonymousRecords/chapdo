9a7a1e13cf2e95e4391ad3d99653c5628263fdcb
Serim Min <97885933+anonymousRecords@users.noreply.github.com>
2024-11-22 00:57:17 +0900

    Miss commits (#4)
    * feat: Implement Carousel
    
    * env: install dependencies
    
    * chore: delete auth page
    
    * chore: Delete unused pages
    
    * style: Add font style
    
    * style: change style of chip
    
    * chore: add link to logo in header
    
    * style: add interactions to bottom-navigation
    
    * feat: implement menu page
    
    * style: change style of expandable navigation
    
    * feat: implement notion renderer
    
    * docs: add posts
    
    * feat: implement til page
    
    * feat: implement post page
    
    * feat: implement temporary projects page
    
    * feat: implement home page
    
    * style: add li to list style none properties
    
    * fix: correct path name
    
    * chore: add metadata to typescript page
    
    * chore: Add missing React import
    
    * chore: add errorboundary to notion renderer

--- apps/frontend/types/menu.ts
+++ apps/frontend/types/menu.ts
@@ -1,7 +1,9 @@
+import { type ComponentType } from 'react';
+
 export interface Menu {
   id: string;
   name: string;
   href: string;
   description?: string;
-  icon?: React.ComponentType;
+  icon?: ComponentType;
 }
