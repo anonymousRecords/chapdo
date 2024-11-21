a091d1b660ad343b4dbaa9953a419da3376e8259
anonymousrecords <a.Records2022@gmail.com>
2024-11-22 00:29:07 +0900

    chore: add link to logo in header

--- apps/frontend/components/layout/header/header.tsx
+++ apps/frontend/components/layout/header/header.tsx
@@ -1,5 +1,5 @@
 import { ArrowLeftIcon, ShareIcon } from '@heroicons/react/24/outline';
-
+import Link from 'next/link';
 interface HeaderProps {
   isDefault?: boolean;
   title?: string;
@@ -7,9 +7,13 @@ interface HeaderProps {
 
 export default function Header({ isDefault = true, title = 'CHAPDO' }: HeaderProps) {
   return (
-    <header className="fixed top-0 w-full max-w-[600px] z-10 h-[48px] flex items-center justify-between px-4 py-2 bg-white shadow-sm">
+    <header className="fixed top-0 w-full max-w-[600px] z-20 h-[48px] flex items-center justify-between px-4 py-2 bg-white shadow-sm">
       {isDefault ? (
-        <h1 className="text-xl font-bold">{title}</h1>
+        <Link href="/">
+          <h1 className="text-xl font-bold">
+            {title}
+          </h1>
+        </Link>
       ) : (
         <>
           <button className="p-2" aria-label="Go back">
