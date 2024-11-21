9cf4d8785fafef69113ac89789018a367f2f5779
anonymousrecords <a.Records2022@gmail.com>
2024-11-22 00:46:58 +0900

    chore: Add missing React import

--- apps/frontend/types/menu.ts
+++ apps/frontend/types/menu.ts
@@ -1,7 +1,8 @@
+import { type ComponentType } from 'react';
 export interface Menu {
   id: string;
   name: string;
   href: string;
   description?: string;
-  icon?: React.ComponentType;
+  icon?: ComponentType;
 }
